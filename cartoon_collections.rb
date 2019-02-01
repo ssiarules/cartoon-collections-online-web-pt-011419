c4a41094e1cc7dc57d513fbb7a074d7ab72266d7
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-01 06:32:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+ def roll_call_dwarves(dwarves) #code an argument here
+   counter = 1 
+   dwarves.each do |dwarves|
+   puts "#{counter}. #{dwarves}"  # Your code here
+   counter += 1 
+ end
+end 
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+def summon_captain_planet(veggies)# code an argument here
+  veggies.each do |veggies|
+  puts  "#{veggies}" # Your code here
+ end 
+end 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+#the rest of the methods I can't get 
+ 
+ def summon_captain_planet(fruits)
+   #.map(&:upcase)
+  fruits.map do |fruits| fruits.upcase + "!"
+ end 
+ fruits 
+end 
 
-def find_the_cheese# code an argument here
+#begin def long_planeteer_calls# code an argument here
+  # Your code here
+#end
+
+#def find_the_cheese# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  #cheese_types = ["cheddar", "gouda", "camembert"]
+#end
+
+ 
+
